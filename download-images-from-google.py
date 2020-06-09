471c4c4c1513ab3dd6eb1ff6be6b61fe3788d68e
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2020-06-09 10:54:57 +0530

    Download images from google
    You can also contribute your code. Fork my repo to your repo.
    
    Let's code

---
+++ download-images-from-google.py
@@ -0,0 +1,31 @@
+# Bhavesh Padharia
+# Download images from google using python
+
+import requests
+import shutil
+import tkinter as tk
+import tkinter.tkk as tkk
+from tkinter.messagebox import showinfo
+
+win = tk.TK()
+win.title("Image downloader...")
+
+def download():
+    url = entry.get()
+    img = requests.get(url,stream=True)
+    saveImg = open(r'big data',wb)
+    shutil.copyfileobj(img.raw,saveImg)
+    showinfo("done","image download successfully")
+
+label = tk.Label(win,text='Enter the url here: ')
+label.grid(row=0,column=0,padx=5,pady=5)
+
+entry = tk.Entry(win,width=30)
+entry.grid(row=0,column=1,padx=5,pady=5)
+
+button = ttk.Button(win,text="Download",command=download)
+button.grid(row=1,column=0,columnspan=2,padx=5,pady=5)
+
+win.mainloop()
+
+
