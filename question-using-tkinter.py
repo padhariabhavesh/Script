085d9f4688e7d66977a9052e28cd8148db53ce7b
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2020-06-26 22:43:44 +0530

    messagebox

---
+++ question-using-tkinter.py
@@ -0,0 +1,33 @@
+# Programmer : Bhavesh Padharia
+
+# Question ask using tkinter in Python
+
+from tkinter import *
+from tkinter import messagebox
+
+# Object of Tk()
+
+main = Tk()
+
+#function to use the ask question
+
+def Submit():
+    messagebox.askquestion("From", "Do you want to submit")
+
+#setting geometry of window instance
+
+main.geometry("100x100")
+#creating window
+
+B1 = Button(main, text="Submit", command=Submit)
+
+#Button positioning
+
+B1.pack()
+
+#infinite loop till close
+
+main.mainloop()
+
+
+
