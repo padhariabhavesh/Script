9d1fc15be7be66ad80e840b07448c1e8b0794bda
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2020-06-26 11:22:16 +0530

    Update pdf-to-text.py

--- pdf-to-text.py
+++ pdf-to-text.py
@@ -1,27 +1,13 @@
 # # Programmer Bhavesh Padharia
 # # PDF to Text converter
-#
-# import PyPDF2
-# pdffileobj=open('padharia.pdf','rb')
-# pdfreader=PyPDF2.PdffileReader(pdffileobj)
-# x=pdfreader.numPages
-# pageobj=pdfreader.getPage(x-1)
-# text=pageobj.extractText()
-#
-# file1=open(r"(/home/bhavesh/PycharmProjects/github/Scripts/)\\padharia.txt","a")
-# file1.writelines(text)
-# file1.close()
-
-
-
 
 import PyPDF2
-pdffileobj=open('1.pdf','rb')
+pdffileobj=open('pdfpath.pdf','rb')
 pdfreader=PyPDF2.PdfFileReader(pdffileobj)
 x=pdfreader.numPages
 pageobj=pdfreader.getPage(x-1)
 text=pageobj.extractText()
 
-file1=open(r"/home/bhavesh/PycharmProjects/github/Scripts/1.txt","a")
+file1=open(r"/pathtosave/1.txt","a")
 file1.writelines(text)
 file1.close()
